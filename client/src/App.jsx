353f4c7fe9eefdfb1a8a91ba8205a3835b83b523
sam <s.clarkyy.1994@gmail.com>
2025-09-18 15:31:39 +0100

    added the components i think i will need

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,3 @@
-//!!!!!! please, please, please, split your code into components
-
-// - Start with your wireframe: you build your React client based on the UI
-
 import "./App.css";
 
 export default function App() {
@@ -23,32 +19,6 @@ export default function App() {
   return (
     <>
       <h1>Gallery</h1>
-      <div>
-        This is a container for my thumbnail images. Here I will render a list
-        of images and I will give each of them a key.
-      </div>
-
-      <ul>
-        <li>Other elements:</li>
-        <li>A method to render all images</li>
-        <li>
-          An event to click on the images. The event handler here is how we
-          handle switching between images
-        </li>
-      </ul>
-      {/* //======================================================= */}
-      <div>
-        This is a container for my larger images! This container is
-        conditionally rendered!
-      </div>
-      <ul>
-        <li>Other elements: </li>
-        <li>An element to contain the larger image</li>
-        <li>
-          Some conditional logic to render this element once the user has
-          clicked on the corresponding thumbnail
-        </li>
-      </ul>
     </>
   );
 }
