f4e8871d9edab1ec878b3395d9065e68c3af3348
sam <s.clarkyy.1994@gmail.com>
2025-09-19 20:37:09 +0100

    🤞🤞🤞 3RD TIMES THE CHARM 🤞🤞🤞

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -2,15 +2,12 @@ import "./Footer.css";
 
 export default function Footer() {
   return (
-    <footer
-      id="footer"
-      className="bg-[#1D8D84] p-2 text-center absolute inset-x-0 bottom-0 border-t-2 border-black"
-    >
-      <p className="text-black text-base">
+    <footer id="footer">
+      <p>
         Quote of the day: The strongest people are not those who show strength
         in front of us but those who win battles we know nothing about
       </p>
-      <p className="text-black text-sm">Made by S P J Clark &copy; 2025</p>
+      <p>Made by S P J Clark &copy; 2025</p>
     </footer>
   );
 }
